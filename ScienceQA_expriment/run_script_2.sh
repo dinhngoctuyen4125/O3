a8590b5f38c971ca672401646c3f2a72ed335927
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-07-22 01:42:56 +0700

    dffd

---
+++ ScienceQA_expriment/run_script_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "===== HOST ====="
+hostname
+
+echo "===== GPU ====="
+nvidia-smi
+
+echo "===== PYTORCH ====="
+
+python -c "import torch; print(torch.__version__); print(torch.version.cuda); print(torch.cuda.is_available()); print(torch.cuda.device_count())"
