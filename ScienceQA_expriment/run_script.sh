9b2ff51d99106f6decd0d2d3c6486778f6ad7adb
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-07-21 21:31:42 +0700

    lmao

--- ScienceQA_expriment/run_script.sh
+++ ScienceQA_expriment/run_script.sh
@@ -3,22 +3,32 @@
 #SBATCH --time=240:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=80G
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=30          # khoảng 10 CPU/GPU
+#SBATCH --mem=120G                  # có thể điều chỉnh
+#SBATCH --gres=gpu:3
 #SBATCH --output=/home/user03/tuyen/O3/ScienceQA_expriment/logs/%x_%j.out
 #SBATCH --error=/home/user03/tuyen/O3/ScienceQA_expriment/logs/%x_%j.err
 
-echo "===== Host ====="
-hostname
+cd /home/user03/tuyen/O3/ScienceQA_expriment
 
-echo "===== GPU ====="
-which nvidia-smi
-nvidia-smi
-
-echo "===== CUDA ====="
-python -c "import torch; \
-print('CUDA available:', torch.cuda.is_available()); \
-print('CUDA version:', torch.version.cuda); \
-print('GPU count:', torch.cuda.device_count()); \
-print('GPU name:', torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'None')"
+torchrun \
+    --nproc_per_node=3 \
+    --nnodes=1 \
+    train_basemodel.py \
+    --model_name_or_path NousResearch/Llama-2-7b-hf \
+    --data_path ./data/qa_all/qaall_train.json \
+    --bf16 True \
+    --output_dir O3_LLAMA2_ScienceQA \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy no \
+    --save_strategy steps \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0 \
+    --warmup_ratio 0.03 \
+    --deepspeed ./configs/default_offload_opt_param.json \
+    --tf32 True
