f881921d2492e89e784f81372d4b3f128f377d63
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-07-22 01:48:35 +0700

    fddf

--- ScienceQA_expriment/run_script_2.sh
+++ ScienceQA_expriment/run_script_2.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
+#SBATCH --job-name=Tuyen
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --mem=120G
+#SBATCH --gres=gpu:2
 #SBATCH --output=/home/user03/tuyen/O3/ScienceQA_expriment/logs/%x_%j.out
 #SBATCH --error=/home/user03/tuyen/O3/ScienceQA_expriment/logs/%x_%j.err
 
 echo "===== HOST ====="
 hostname
 
+echo "===== CUDA_VISIBLE_DEVICES ====="
+echo "$CUDA_VISIBLE_DEVICES"
+
 echo "===== GPU ====="
+which nvidia-smi
 nvidia-smi
 
 echo "===== PYTORCH ====="
+python -c "import torch; print('Torch:', torch.__version__); print('CUDA build:', torch.version.cuda); print('CUDA available:', torch.cuda.is_available()); print('Device count:', torch.cuda.device_count())"
 
-python -c "import torch; print(torch.__version__); print(torch.version.cuda); print(torch.cuda.is_available()); print(torch.cuda.device_count())"
+echo "===== DEVICES ====="
+ls -l /dev/nvidia*
